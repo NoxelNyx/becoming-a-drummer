170d68a35657ecb23f89b17cf0a5db1f6391fa7e
NoxelNyx <tnova@azdes.gov>
2023-08-01 15:17:10 -0700

    better loading bar

--- src/firebase/provider.tsx
+++ src/firebase/provider.tsx
@@ -6,6 +6,7 @@ import {
 } from 'firebase/auth';
 import firebase_app from './config';
 import Logo from '@/src/components/Logo';
+import { LinearProgress } from '@mui/material';
 
 export const auth = getAuth(firebase_app);
 export const AuthContext = React.createContext<User | null>(null);
@@ -32,11 +33,14 @@ export const AuthContextProvider = ({
     return (
         <AuthContext.Provider value={user}>
             {loading 
-                ? <div style={{ backgroundColor: '#121212', height: '100vh' }}>
-                    <div style={{ position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)' }}>
-                        <Logo sizes="100vh" height={500} className='mx-auto' />
+                ? <React.Fragment>
+                    <div style={{ backgroundColor: '#121212', height: '100vh' }}>
+                        <LinearProgress color='secondary' />
+                        <div style={{ position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)' }}>
+                            <Logo sizes="100vh" height={500} className='mx-auto' />
+                        </div>
                     </div>
-                </div>
+                </React.Fragment>
                 : children}
         </AuthContext.Provider>
     );
