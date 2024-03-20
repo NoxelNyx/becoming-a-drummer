b418651384976a5126598893ee56fb52bfad5d85
NoxelNyx <noxelnyx@gmail.com>
2024-03-20 13:41:39 -0700

    YT searches should now be more relevant

--- src/components/SearchBar/api.tsx
+++ src/components/SearchBar/api.tsx
@@ -1,6 +1,6 @@
 export async function searchGoogle(query: string) {
     const yt_api_key = localStorage.getItem('yt_api_key') ?? process.env.NEXT_PUBLIC_YT_API_KEY;
-    const res = await fetch('https://www.googleapis.com/youtube/v3/search?q=' + query + '&part=snippet&maxResults=50&type=video&key=' + yt_api_key);
+    const res = await fetch('https://www.googleapis.com/youtube/v3/search?q=' + query + ' official&part=snippet&maxResults=50&type=video&key=' + yt_api_key);
 
     return res.json();
 }
